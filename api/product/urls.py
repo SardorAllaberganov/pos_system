e99d480ccc957517268fe5a0c3220f49face7619
Sardor Allaberganov <sardorallaberganov96@gmail.com>
2024-09-08 22:56:08 +0500

    get one product and paginator for products

--- api/product/urls.py
+++ api/product/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from .views import all_products
+from .views import all_products, product_detail
 
 urlpatterns = [
     path('/all', all_products, name='all_products'),
+    path('/<int:product_id>', product_detail, name='product_detail')
 ]
-
-
--- api/product/views.py
+++ api/product/views.py
@@ -4,10 +4,24 @@ from rest_framework.permissions import AllowAny, IsAuthenticated
 
 from api.product.models import Product
 from .serializers import ProductSerializer
+from api.core.paginator import CustomPagination
 
 @api_view(['GET'])
 @permission_classes([AllowAny])
 def all_products(request):
     products = Product.objects.all()
-    serializer = ProductSerializer(products, many=True)
+    paginator = CustomPagination()
+    paginated_products = paginator.paginate_queryset(products, request)
+    serializer = ProductSerializer(paginated_products, many=True)
     return Response({"message": "All products fetched successfully", "data": serializer.data}, status=200)
+
+@api_view(['GET'])
+@permission_classes([AllowAny])
+def product_detail(request, product_id):
+    try:
+        product = Product.objects.get(pk=product_id)
+    except Product.DoesNotExist:
+        return Response({"message": "Product not found"}, status=404)
+
+    serializer = ProductSerializer(product)
+    return Response({"message": "Product fetched successfully", "data": serializer.data}, status=200)
