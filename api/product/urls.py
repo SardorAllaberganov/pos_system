3bdfd3a48fbb392a073cb14acf084bfa5654cfbb
SardorAllaberganov <sardorallaberganov96@gmail.com>
2024-09-09 15:57:05 +0500

    product delete

--- api/product/urls.py
+++ api/product/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import all_products, product_detail
+from .views import all_products, product_detail, delete_product
 
 urlpatterns = [
     path('/all', all_products, name='all_products'),
-    path('/<int:product_id>', product_detail, name='product_detail')
+    path('/<int:product_id>', product_detail, name='product_detail'),
+    path('/delete/<int:product_id>', delete_product, name='delete_product')
 ]
--- api/product/views.py
+++ api/product/views.py
@@ -5,6 +5,8 @@ from rest_framework.permissions import AllowAny, IsAuthenticated
 from api.product.models import Product
 from .serializers import ProductSerializer
 from api.core.paginator import CustomPagination
+from api.core.decorators import check_role
+
 
 @api_view(['GET'])
 @permission_classes([AllowAny])
@@ -14,6 +16,7 @@ def all_products(request):
     paginated_products = paginator.paginate_queryset(products, request)
     serializer = ProductSerializer(paginated_products, many=True)
     return Response({"message": "All products fetched successfully", "data": serializer.data}, status=200)
+
 
 @api_view(['GET'])
 @permission_classes([AllowAny])
@@ -25,3 +28,16 @@ def product_detail(request, product_id):
 
     serializer = ProductSerializer(product)
     return Response({"message": "Product fetched successfully", "data": serializer.data}, status=200)
+
+
+@api_view(['DELETE'])
+@permission_classes([IsAuthenticated])
+@check_role(["Admin", "Manager"])
+def delete_product(request, product_id):
+    if request.method == "DELETE":
+        try:
+            product = Product.objects.get(pk=product_id)
+            product.delete()
+            return Response({"message": "Product deleted successfully"}, status=200)
+        except Product.DoesNotExist:
+            return Response({"message": "Product not found"}, status=404)
