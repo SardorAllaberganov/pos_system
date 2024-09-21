0a244335f5801105ab54dc8510ab76c99a56150f
Sardor Allaberganov <sardorallaberganov96@gmail.com>
2024-09-21 22:28:57 +0500

    customer crud apis

--- api/customer/urls.py
+++ api/customer/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import all_customers
+from .views import all_customers, customer_detail, create_customer, update_customer, delete_customer
 
 urlpatterns = [
     path('/all', all_customers, name='all_customers'),
+    path('/<int:customer_id>', customer_detail, name='customer_detail'),
+    path('/create', create_customer, name='create_customer'),
+    path('/update/<int:customer_id>', update_customer, name='update_customer'),
+    path('/delete/<int:customer_id>', delete_customer, name='delete_customer'),
 ]
--- api/customer/views.py
+++ api/customer/views.py
@@ -6,7 +6,6 @@ from .serializers import CustomerSerializer
 from django.db.models import Q
 from api.core.paginator import CustomPagination
 
-
 @api_view(['GET'])
 @permission_classes([AllowAny])
 def all_customers(request):
@@ -22,3 +21,51 @@ def all_customers(request):
 
     serializer = CustomerSerializer(paginated_customers, many=True)
     return Response({"message": "Successfully fetched all customers", "data": serializer.data}, status=200)
+
+@api_view(['GET'])
+@permission_classes([AllowAny])
+def customer_detail(request, customer_id):
+    try:
+        customer = Customer.objects.get(pk=customer_id)
+    except Customer.DoesNotExist:
+        return Response({"message": "Customer not found"}, status=404)
+
+    serializer = CustomerSerializer(customer)
+    return Response({"message": "Successfully fetched customer", "data": serializer.data}, status=200)
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def create_customer(request):
+    if request.method == 'POST':
+        serializer = CustomerSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"message": "Customer created", "data": serializer.data}, status=201)
+        else:
+            return Response({"error": serializer.errors}, status=400)
+
+@api_view(['PUT'])
+@permission_classes([IsAuthenticated])
+def update_customer(request, customer_id):
+    if request.method == 'PUT':
+        try:
+            customer = Customer.objects.get(pk=customer_id)
+        except Customer.DoesNotExist:
+            return Response({"message": "Customer not found"}, status=404)
+        serializer = CustomerSerializer(customer, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"message": "Successfully updated customer", "data": serializer.data}, status=200)
+        else:
+            return Response({"error": serializer.errors}, status=400)
+
+
+@api_view(['DELETE'])
+@permission_classes([IsAuthenticated])
+def delete_customer(request, customer_id):
+    try:
+        customer = Customer.objects.get(pk=customer_id)
+        customer.delete()
+        return Response({"message": "Customer deleted", "data": None}, status=200)
+    except Customer.DoesNotExist:
+        return Response({"message": "Customer not found"}, status=404)
